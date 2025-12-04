517fa2056cc1b03595b4e4eea1c268863f610c7d
HarshitKumarSahu <poltuharshit14@gmail.com>
2025-12-04 17:46:08 +0530

    pages

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,19 @@
-// vite.config.js
-import { defineConfig } from 'vite';
-import glsl from 'vite-plugin-glsl';
+// // vite.config.js
+// import { defineConfig } from 'vite';
+// import glsl from 'vite-plugin-glsl';
 
-export default defineConfig({
-  plugins: [glsl()]
-});
+// export default defineConfig({
+//   plugins: [glsl()]
+// });
+
+export default {
+    build: {
+        rollupOptions: {
+            input: {
+                main: './index.html',
+                explore: './explore.html',
+                gsplat: './gsplat.html',
+            }
+        }
+    }
+};
