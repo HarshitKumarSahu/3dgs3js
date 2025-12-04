2afa4aff16b36438136db21324401f7d36923750
HarshitKumarSahu <poltuharshit14@gmail.com>
2025-12-04 11:50:47 +0530

    write up code need to fix some thing

---
+++ src/handGsplat.js
@@ -0,0 +1,36 @@
+import * as SPLAT from "gsplat";
+
+const canvas = document.getElementById("gsplat-hand");
+const progressDialog = document.getElementById("progress-dialog");
+const progressIndicator = document.getElementById("progress-indicator");
+
+const renderer = new SPLAT.WebGLRenderer(canvas);
+const scene = new SPLAT.Scene();
+const camera = new SPLAT.Camera();
+// const controls = new SPLAT.OrbitControls(camera, canvas);
+const controls = new SPLAT.FPSControls(camera, canvas);
+
+async function main() {
+    const url = "https://huggingface.co/datasets/poltu14/3dgsplat/resolve/main/hand.splat"
+    
+    await SPLAT.Loader.LoadAsync(url, scene, (progress) => (progressIndicator.value = progress * 100));
+    progressDialog.close();
+
+    const handleResize = () => {
+        renderer.setSize(canvas.clientWidth, canvas.clientHeight);
+    };
+
+    const frame = () => {
+        controls.update();
+        renderer.render(scene, camera);
+
+        requestAnimationFrame(frame);
+    };
+
+    handleResize();
+    window.addEventListener("resize", handleResize);
+
+    requestAnimationFrame(frame);
+}
+
+main();
